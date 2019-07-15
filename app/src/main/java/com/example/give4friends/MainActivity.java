732928c980d4056ce5e2e91db1650db4025b87c7
Babu12345 <bwanyeki12@gmail.com>
2019-07-15 12:06:22 -0700

    Testing the branch system

--- app/src/main/java/com/example/give4friends/MainActivity.java
+++ app/src/main/java/com/example/give4friends/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private void testMethod(){
+
+        
+    }
 }
