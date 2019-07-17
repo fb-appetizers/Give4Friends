23604db246daa5e40994de5065953410b295b251
Hana Tadesse <hanat18@fb.com>
2019-07-17 16:22:37 -0700

    plus btn in homepage

--- app/src/main/java/com/example/give4friends/CharitySearch.java
+++ app/src/main/java/com/example/give4friends/CharitySearch.java
@@ -1,7 +1,9 @@
 package com.example.give4friends;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,6 +20,7 @@ import android.widget.Toast;
 import com.example.give4friends.Adapters.CharitySearchAdapter;
 import com.example.give4friends.models.CharityAPI;
 import com.example.give4friends.net.CharityClient;
+import com.parse.ParseUser;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,8 +33,6 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class CharitySearch extends AppCompatActivity {
-
-
     private EditText etCharity;
     private RecyclerView rvCharitySearch;
     private Button btnSubmit;
@@ -45,6 +46,8 @@ public class CharitySearch extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_charity_search);
 
+        configureToolbar();
+
         etCharity = findViewById(R.id.etCharity);
         rvCharitySearch = findViewById(R.id.rvCharitySearch);
         etCharity = findViewById(R.id.etCharity);
@@ -53,6 +56,7 @@ public class CharitySearch extends AppCompatActivity {
         acharities = new ArrayList<CharityAPI>();
 
         charityAdapter = new CharitySearchAdapter(acharities);
+
 
 
         // attach the adapter to the RecyclerView
@@ -79,15 +83,6 @@ public class CharitySearch extends AppCompatActivity {
 
 
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.charity_menu, menu);
-
-        return true;
-    }
-
 
 
     private void getResponse(String search, boolean search_by_name){
@@ -141,17 +136,67 @@ public class CharitySearch extends AppCompatActivity {
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-
                         }
                     });
-
-
-
-
                 }
             }
         });
 
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Give4Friends");
+        setSupportActionBar(toolbar);
+
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CharitySearch.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.charity_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.etCharity:
+                Toast.makeText(this, "Charity Search selected", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(CharitySearch.this, CharitySearch.class);
+                startActivity(intent);
+                return true;
+            case R.id.transactionHistory:
+                Toast.makeText(this, "Transaction History selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.settings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.logOut:
+                Toast.makeText(this, "logging out...", Toast.LENGTH_LONG).show();
+                logOut();
+//            case R.id.toolbar
+            default:
+//                Log.e()
+        }
+        return true;
+    }
+
+    public void logOut(){
+        ParseUser.logOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
 }
