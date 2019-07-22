a8ddc1773327ee1783a79b40b6eddc2b6bcf00b0
jessica-mccurdy <jessica.mccurdy@yale.edu>
2019-07-22 15:14:55 -0700

    Can now like post and it is updated in parse

--- app/src/main/java/com/example/give4friends/models/Transaction.java
+++ app/src/main/java/com/example/give4friends/models/Transaction.java
@@ -1,5 +1,7 @@
 package com.example.give4friends.models;
 
+import androidx.preference.ListPreference;
+
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
@@ -8,8 +10,11 @@ import com.parse.ParseUser;
 
 
 import org.json.JSONArray;
+import org.json.JSONException;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @ParseClassName("Transaction")
 public class Transaction extends ParseObject{
@@ -102,12 +107,21 @@ public class Transaction extends ParseObject{
         return getNumber(KEY_LIKES_COUNT);
     }
 
-    public void incrementLikes(){
-        increment(KEY_LIKES_COUNT, 1 );
+    public void incrementLikes(int amount){
+        increment(KEY_LIKES_COUNT, amount );
     }
 
-    public JSONArray getKeyLikesUsers() {
-        return getJSONArray(KEY_LIKES_USERS);
+
+    public List getKeyLikesUsers() {
+        return getList(KEY_LIKES_USERS);
+    }
+
+    public void setKeyLikesUsers(List<String> list){
+        put(KEY_LIKES_USERS, list);
+    }
+
+    public void addLikesUser(String user){
+        add(KEY_LIKES_USERS, user);
     }
 
     public Charity getKeyCharityId() {
