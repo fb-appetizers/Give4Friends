b547e7ef8e5c6f0ef86b8d3ee3c7d9ceb053f12d
jessica-mccurdy <jessica.mccurdy@yale.edu>
2019-07-24 11:13:38 -0700

    When you tap profile photo in main feed you are directed to a profile of that user which is not editable but if the photo is of yourself you will go to your editable profile

--- app/src/main/java/com/example/give4friends/Adapters/TransactionAdapter.java
+++ app/src/main/java/com/example/give4friends/Adapters/TransactionAdapter.java
@@ -1,6 +1,7 @@
 package com.example.give4friends.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.view.LayoutInflater;
@@ -16,6 +17,8 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.give4friends.FriendProfileActivity;
+import com.example.give4friends.ProfileActivity;
 import com.example.give4friends.R;
 import com.example.give4friends.models.Transaction;
 import com.example.give4friends.models.User;
@@ -113,36 +116,27 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
                 }
             });
 
-
-
         //populate the views according to this data
-
         holder.message.setText("Message: " + transaction.getKeyMessage());
 
         if( transaction.getKeyCharityId() != null) {
             transaction.getKeyCharityId().fetchIfNeededInBackground(new GetCallback<ParseObject>() {
                 @Override
                 public void done(ParseObject object, ParseException e) {
-
                     holder.charity.setText(object.getString("name"));
                 }
             });
         }
-
-
         transaction.getKeyDonorId().fetchIfNeededInBackground(new GetCallback<ParseObject>() {
             @Override
             public void done(ParseObject object, ParseException e) {
-
                 holder.donor.setText(object.getString("firstName"));
             }
         });
         transaction.getKeyFriendId().fetchIfNeededInBackground(new GetCallback<ParseObject>() {
             @Override
             public void done(ParseObject object, ParseException e) {
-
                 ParseFile image = object.getParseFile("profileImage");
-
                 if(image != null){
                     Glide.with(context)
                             .load(image.getUrl())
@@ -154,6 +148,7 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
                 }
             }
         });
+
         transaction.getKeyDonorId().fetchIfNeededInBackground(new GetCallback<ParseObject>() {
             @Override
             public void done(ParseObject object, ParseException e) {
@@ -175,6 +170,40 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
             @Override
             public void done(ParseObject object, ParseException e) {
                 holder.friend.setText(object.getString("firstName"));
+            }
+        });
+
+        holder.friendPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(transaction.getKeyFriendId().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())   ){
+                    Intent intent = new Intent(context, ProfileActivity.class);
+                    context.startActivity(intent);
+                }
+                else{
+                    Intent intent = new Intent(context, FriendProfileActivity.class);
+                    intent.putExtra("user",transaction.getKeyFriendId() );
+                    context.startActivity(intent);
+
+                }
+
+            }
+        });
+
+        // if yourself send to your profile
+        holder.donorPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(transaction.getKeyDonorId().getObjectId().equals(ParseUser.getCurrentUser().getObjectId())  ){
+                    Intent intent = new Intent(context, ProfileActivity.class);
+                    context.startActivity(intent);
+                }
+                else{
+                    Intent intent = new Intent(context, FriendProfileActivity.class);
+                    intent.putExtra("user",transaction.getKeyDonorId() );
+                    context.startActivity(intent);
+                }
+
             }
         });
     }
@@ -212,16 +241,13 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
             friend= (TextView) itemView.findViewById(R.id.tvFriend);
             charity = (TextView) itemView.findViewById(R.id.tvCharity);
             donorPhoto= (ImageView) itemView.findViewById(R.id.ivDonor);
+            donorPhoto.setClickable(true);
             friendPhoto = (ImageView) itemView.findViewById(R.id.ivFriend);
+            friendPhoto.setClickable(true);
             message = (TextView) itemView.findViewById(R.id.tvMessage);
-
             // like button
             ibEmptyHeart = (ImageButton) itemView.findViewById(R.id.ib_empty_heart);
-
-
-
         }
-
 
         // Clean all elements of the recycler
         public void clear() {
@@ -234,8 +260,6 @@ public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.
             transactions.addAll(list);
             notifyDataSetChanged();
         }
-
-
 
     }
 
---
+++ app/src/main/java/com/example/give4friends/FriendProfileActivity.java
@@ -0,0 +1,268 @@
+package com.example.give4friends;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.give4friends.Adapters.FavCharitiesAdapter;
+import com.example.give4friends.models.Charity;
+import com.example.give4friends.models.ProfilePicture;
+import com.example.give4friends.models.Transaction;
+import com.example.give4friends.models.User;
+import com.parse.FindCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import android.app.AlertDialog;
+
+import android.content.DialogInterface;
+import android.app.AlertDialog.Builder;
+import android.widget.Toast;
+
+import org.parceler.Parcels;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FriendProfileActivity extends AppCompatActivity {
+
+    com.example.give4friends.Adapters.FavCharitiesAdapter feedAdapter;
+    ArrayList<Charity> charities;
+    RecyclerView rvCharities;
+    private SwipeRefreshLayout swipeContainer;
+
+    public ImageView ivProfileImage;
+    public TextView tvUserName;
+    public TextView tvBio;
+    public TextView tvFullName;
+
+
+    ParseUser myUser;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_profile);
+        context = this;
+
+        myUser = getIntent().getParcelableExtra("user");
+
+        configureToolbar();
+
+        //Below for recycler view of charities
+        rvCharities = (RecyclerView) findViewById(R.id.rvFavCharities);
+        // initialize the array list of charities
+        charities = new ArrayList<Charity>();
+        populateRelations();
+        // Lookup the swipe container view
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                feedAdapter.clear();
+                feedAdapter.addAll(charities);
+                populateRelations();
+                swipeContainer.setRefreshing(false);
+            }
+
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+        // Below for static elements of profile
+        ivProfileImage = (ImageView) findViewById((R.id.ivProfileImage));
+        tvUserName = (TextView) findViewById(R.id.tvName);
+        tvBio = (TextView) findViewById(R.id.tvBio);
+        tvFullName = (TextView) findViewById(R.id.tvFullName);
+
+        tvUserName.setText("@" + myUser.getUsername());
+        if(myUser.getString("bio") == null){
+            tvBio.setText(" ");
+        }
+        else{
+            tvBio.setText(myUser.getString("bio"));
+        }
+        tvBio.setEnabled(false);
+        tvFullName.setText(myUser.getString("firstName") + " " + myUser.getString("lastName"));
+
+        //Handles images
+        ParseFile file = myUser.getParseFile("profileImage");
+
+        if (file!=null) {
+            Glide.with(context)
+                    .load(file.getUrl())
+                    .apply(new RequestOptions()
+                            .transforms(new CenterCrop(), new RoundedCorners(20))
+                            .circleCropTransform()
+                            .placeholder(R.drawable.user_outline_24)
+                            .error(R.drawable.user_outline_24))
+                    .into(ivProfileImage);
+        }
+        else{
+            Glide.with(context)
+                    .load(R.drawable.user_outline_24)
+                    .apply(new RequestOptions()
+                            .transforms(new CenterCrop(), new RoundedCorners(20))
+                            .circleCropTransform()
+                            .placeholder(R.drawable.user_outline_24)
+                            .error(R.drawable.user_outline_24))
+                    .into(ivProfileImage);
+        }
+    }
+    //add tool bar
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbarProfile);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayShowTitleEnabled(false);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.charity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.transactionHistory:
+                Toast.makeText(this, "Transaction History selected", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), HistoryActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.useOffline:
+                Toast.makeText(this, "Use Offline selected", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.logOut:
+                Toast.makeText(this, "logging out...", Toast.LENGTH_LONG).show();
+                logOut();
+            default:
+//                Log.e()
+        }
+        return true;
+    }
+
+    public void logOut(){
+        ParseUser.logOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+    private void populateRelations() {
+        //Get relation
+        final ParseRelation<Charity> favCharities = myUser.getRelation("favCharities");
+        //Get all charities in relation
+        favCharities.getQuery().findInBackground(new FindCallback<Charity>() {
+            @Override
+            public void done(List<Charity> objects, ParseException e) {
+                if (e != null) {
+                    // There was an error
+                } else {
+                    // results have all the charities the current user liked.
+                    // go through relation adding charities
+                    for (int i = 0; i < objects.size(); i++) {
+                        charities.add((Charity) objects.get(i));
+
+                    }
+                    final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(FriendProfileActivity.this);
+                    rvCharities.setLayoutManager(linearLayoutManager);
+
+                    //construct the adapter from this datasource
+                    feedAdapter = new FavCharitiesAdapter(charities);
+                    //RecyclerView setup (layout manager, use adapter)
+                    rvCharities.setAdapter(feedAdapter);
+                    rvCharities.scrollToPosition(0);
+
+                }
+            }
+        });
+
+    }
+
+}
+
+
+
+
+
+
+// sample popup with edit textbox
+/*
+    private void showAddItemDialog(Context c) {
+        final EditText taskEditText = new EditText(c);
+        AlertDialog dialog = new AlertDialog.Builder(c)
+                .setTitle("Change Profile Picture")
+                .setView(taskEditText)
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEditText.getText());
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
+    }
+
+*/
+
