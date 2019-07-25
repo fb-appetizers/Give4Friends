0f32482f42e362fd8104a8bb30189f15ab34a252
Hana Tadesse <hanat18@fb.com>
2019-07-24 17:37:48 -0700

    Credit Card Info Page added

--- app/src/main/java/com/example/give4friends/DonateSearchCharity.java
+++ app/src/main/java/com/example/give4friends/DonateSearchCharity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
