e1484f4ad0611d9ce23f1714f4e5e4b60427cbba
Babu12345 <bwanyeki12@gmail.com>
2019-07-31 10:48:19 -0700

    Updated the favorites logic

--- app/src/main/java/com/example/give4friends/models/FavoriteCharities.java
+++ app/src/main/java/com/example/give4friends/models/FavoriteCharities.java
@@ -28,6 +28,8 @@ public final class FavoriteCharities {
         //check if user is in likes list
         List<User> array = parseCharity.getList("likesUsers");
 
+
+
         if (array == null || !(array.contains(myUser.getObjectId()))) {
 
             ibCPLike.setImageResource(R.drawable.ic_like_icon);
