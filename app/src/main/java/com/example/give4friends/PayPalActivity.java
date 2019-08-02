1049ab233afbc31233992bfc3cf98dd55b4710a2
jessica-mccurdy <jessica.mccurdy@yale.edu>
2019-08-01 17:28:20 -0700

    redesigned favorite charities item

--- app/src/main/java/com/example/give4friends/PayPalActivity.java
+++ app/src/main/java/com/example/give4friends/PayPalActivity.java
@@ -41,9 +41,10 @@ public class PayPalActivity extends AppCompatActivity {
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.loadUrl(url);
-        webView.loadUrl("javascript:(function() { document.getElementById('nemo_inputAmount').value = '" + amount + "'; ;})()");
-
         webSettings.setUserAgentString("Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3");
+        webView.loadUrl("javascript:(function() { document.getElementById('nemo_inputAmount').value = 'amount'; null})()");
+
+
 
 
         webView.canGoBack();
