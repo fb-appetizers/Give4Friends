8e1fe7e8df124e5c049404bd62a3816ca6751ac0
Hana Tadesse <hanat18@fb.com>
2019-08-07 16:53:07 -0700

    Splash Screen and new Logo added

---
+++ app/src/main/java/com/example/give4friends/Splash.java
@@ -0,0 +1,29 @@
+package com.example.give4friends;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splash.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+
+    }
+}
